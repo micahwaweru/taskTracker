424180193fd2fe03f10c8018bdb6369bf44f8bb9
Micah Waweru <micah.waweru@gmail.com>
2022-06-17 11:18:55 -0500

    morning work

--- public/script.js
+++ public/script.js
@@ -5,6 +5,7 @@ var formDisp = document.getElementById('formDisp');
 var taskFormCont = document.getElementById('taskFormCont');
 var taskForm = document.getElementById('taskForm');
 var submit = document.getElementById('submit');
+var taskList = document.getElementById('taskList');
 
 
 var removeHide = function(element){
@@ -15,19 +16,86 @@ var removeHide = function(element){
 var taskFormHandler = function(){
     console.log(`${taskForm.taskTitle.value} ${taskForm.taskDesc.value}`)
     var formValues = {
-        titleObj: taskForm.taskTitle.value,
-        descObj: taskForm.taskDesc.value
+        title: taskForm.taskTitle.value,
+        desc: taskForm.taskDesc.value
     }
     console.log(formValues);
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST','/addTask',true);
+    xhr.setRequestHeader('Content-Type','application/json');
+    xhr.send(JSON.stringify(formValues))
     
 }
+
+var elBuilder = function(obj){
+    for(i=0;i<obj.length;i++){
+        console.log(obj[i].title)
+        var taTi = obj[i].title;
+        var taDe=obj[i].desc;
+        var taId=obj[i].id;
+
+        var listCard = document.createElement('div')
+        listCard.classList.add('card')
+        taskList.appendChild(listCard)
+
+            var btnDiv = document.createElement('div')
+            btnDiv.classList.add('card-header', 'd-flex','flex-row','mb-2')
+            listCard.appendChild(btnDiv)
+            var btnEl=document.createElement('button')
+            btnEl.innerHTML = "Done";
+            btnEl.classList.add('btn','btn-primary');
+            btnEl.setAttribute('id',`${taId}`)
+            btnDiv.appendChild(btnEl);
+            var heEl=document.createElement('h3');
+            heEl.innerHTML=taTi;
+            heEl.classList.add('p-2');
+            btnDiv.appendChild(heEl);
+
+            var desDiv=document.createElement('div')
+            desDiv.classList.add('card-body')
+            listCard.appendChild(desDiv)
+            var blk=document.createElement('blockquote')
+            blk.classList.add('blockquote','mb-0')
+            desDiv.appendChild(blk)
+            var desTxt=document.createElement('p')
+            desTxt.innerHTML= taDe;
+            desDiv.appendChild(desTxt);
+
+
+
+
+    }
+}
+
+var taskPop = function(){
+    fetch('/viewTasks',{
+        'method':'GET',
+        'headers':{
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(
+        function(response){
+            response.json().then(function(data){
+                console.log(data);
+                elBuilder(data)
+        
+            })
+        }
+    )
+}
+
+
+
+
+
 
 formDisp.addEventListener('click', function(){
     removeHide(taskFormCont)
 });
 
-submit.addEventListener('click', function(e){
-    e.preventDefault();
+submit.addEventListener('click', function(){
     taskFormHandler();
 })
 
+taskPop();
--- server.js
+++ server.js
@@ -1,17 +1,74 @@
 const express = require('express');
 
+
 const fs = require('fs');
 const path = require('path');
+const { stringify } = require('querystring');
+
 const PORT = 4001;
 const app = express();
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.json);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'./public/index.html'));
 });
+
+
+app.get('/viewTasks',(req,res)=>{
+    fs.readFile('db.json',(err,data)=>{
+        if(err)throw err;
+        dbData=JSON.parse(data);
+        console.log(dbData);
+        res.send(dbData);
+    });
+});
+
+app.post('/addTask',(req,res)=>{
+    const newTask = req.body;
+    fs.readFile('db.json',(err,data)=>{
+        if(err)throw err;
+        dbData=JSON.parse(data);
+        dbData.push(newTask)
+        let number = 1;
+            dbData.forEach((task,i)=>{
+                task.id = number;
+                number++
+                return dbData;
+            });
+        stringData=JSON.stringify(dbData);
+        fs.writeFile('db.json',stringData,(err,data)=>{
+            if(err)throw err;
+        });
+    });
+    res.send('Create Route')
+});
+
+app.delete('/deleteTask/:id',(req,res)=>{
+    var taskId=req.params.id;
+    fs.readFile('db.json',(err,data)=>{
+        if(err)throw err;
+        dbData=JSON.parse(data);
+        for(let i=0;i<dbData.length;i++){
+            if(dbData[i].id===Number(taskId)){
+                dbData.splice([i],1);
+            }
+        }
+        stringData=JSON.stringify(dbData);
+        fs.writeFile('db.json',stringData,(err)=>{
+            if(err)throw err;
+        });
+    });
+    res.send(`Task number ${taskId} deleted`)
+})
+
+
+
+
+
+
 
 
 
